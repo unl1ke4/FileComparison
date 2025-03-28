8144bb0348ff4af4f831a87ab766c00ac3d46f54
makskorney <maks32273@gmail.com>
2025-03-28 18:41:22 +0200

    додав фікстуру та тест count_sentence_endings

--- test_middletest.py
+++ test_middletest.py
@@ -4,6 +4,11 @@ from io import StringIO
 
 from middletest import read_file, count_words, count_sentences, count_sentence_endings, count_punctuation_marks
 
+# Фікстура для підготовки тексту
+@pytest.fixture
+def sample_text():
+    return "Привіт! Це тестовий текст. Він має кілька слів, речень і розділювальних знаків."
+
 # count_punctuation_marks
 @pytest.mark.parametrize("text, expected", [
     ("Привіт! Це тестовий текст.", 2),
@@ -12,3 +17,12 @@ from middletest import read_file, count_words, count_sentences, count_sentence_e
 ])
 def test_count_punctuation_marks(text, expected):
     assert count_punctuation_marks(text) == expected
+    
+# count_sentence_endings
+@pytest.mark.parametrize("text, expected", [
+    ("Привіт! Це тестовий текст.", 2),
+    ("", 0),
+    ("Тест.", 1)
+])
+def test_count_sentence_endings(text, expected):
+    assert count_sentence_endings(text) == expected
