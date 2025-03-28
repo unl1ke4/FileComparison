4da14694ba9c412aa39816f6945c4fd416161b45
pivassio <akeno.harada1996@gmail.com>
2025-03-28 19:27:31 +0200

    Додав тести

--- test_middletest.py
+++ test_middletest.py
@@ -1,6 +1,7 @@
 import pytest
 from unittest.mock import patch 
 from middletest import read_file, count_words, count_sentences, count_sentence_endings, count_punctuation_marks, analyze_text
+from io import StringIO
 
 # Фікстура для підготовки тексту
 @pytest.fixture
@@ -16,6 +17,24 @@ def sample_text():
 def test_count_punctuation_marks(text, expected):
     assert count_punctuation_marks(text) == expected
 
+# count_words
+@pytest.mark.parametrize("text, expected", [
+    ("Привіт світ", 2),
+    ("Тест, який має кілька слів.", 5),
+    ("", 0)
+])
+def test_count_words(text, expected):
+    assert count_words(text) == expected
+    
+# count_sentences
+@pytest.mark.parametrize("text, expected", [
+    ("Привіт! Це тестовий текст.", 2),
+    ("", 0),
+    ("Тест.", 1)
+])
+def test_count_sentences(text, expected):
+    assert count_sentences(text) == expected
+
 # count_sentence_endings
 @pytest.mark.parametrize("text, expected", [
     ("Привіт! Це тестовий текст.", 2),
@@ -28,3 +47,8 @@ def test_count_sentence_endings(text, expected):
 def test_analyze_text():
     with patch('builtins.input', return_value='example.txt'):
         analyze_text()
+
+# read_file
+def test_read_file(sample_text):
+    with patch("builtins.open", return_value=StringIO(sample_text)):
+        assert read_file("fake_path.txt") == sample_text
